05c70b0355fb8ead04c26e5b356cd27531d9fee9
Fatih Seker <fatihzucker@gmail.com>
2023-04-05 02:46:59 +0200

    some changes

---
+++ src/test/java/com/cydeo/day4/CBTrainingApiWithJsonPath.java
@@ -0,0 +1,54 @@
+package com.cydeo.day4;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CBTrainingApiWithJsonPath {
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://api.training.cydeo.com";
+
+    }
+
+    @DisplayName("GET Request to individual student")
+    @Test
+    public void test1(){
+//send a get request to student id 23401 as a path parameter and accept header application/json
+        //verify status code=200 /content type=application/json;charset=UTF-8 /Content-Encoding = gzip
+        //verify Date header exists
+        //assert that
+            /*
+                firstName Vera
+                batch 14
+                section 12
+                emailAddress aaa@gmail.com
+                companyName Cybertek
+                state IL
+                zipCode 60606
+
+                using JsonPath
+             */
+
+
+
+    }
+
+
+}
